794afb5104a5f4a82fdce2992c4294431bc4a0d0
Gerson-David <gersondavid106@gmail.com>
2020-05-01 17:51:58 -0300

    Create IMC.py

---
+++ IMC.py
@@ -0,0 +1,14 @@
+peso = float(input('Digite seu peso (Kg): '))
+altu = float(input('Digite sua altura: '))
+imc = peso / (altu ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal!')
+elif imc >= 18.5 and imc < 25:
+    print('Parabéns, você está na faixa de PESO NORMAL!')
+elif imc >= 25 and imc < 30:
+    print('Você está em SOBREPESO')
+elif imc >= 30 and imc < 40:
+    print('ATENÇÃO, Você está com OBESIDADE')
+else:
+    print('CUIDADO, você está com OBESIDADE MORBIDA!')
